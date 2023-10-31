8ad033d1a237eefe00a4a7aecb6c3abe648b58c5
Saflxa <133849314+Saflxa@users.noreply.github.com>
2023-10-31 18:46:50 +0700

    Add files via upload

---
+++ UTS/bobot.sql
@@ -0,0 +1,13 @@
+INSERT INTO uts_spk.bobot (nama,clock_speed,bandwidth,vram,harga,series,bobot) VALUES
+	 ('NVIDIA GeForce RTX 3060 Ti','3','2','3','3','4',NULL),
+	 ('NVIDIA GeForce RTX 3060','2','3','3','3','3',NULL),
+	 ('NVIDIA GeForce RTX 3050','3','2','3','4','1',NULL),
+	 ('NVIDIA GeForce GTX 1660 Ti','3','2','2','4','4',NULL),
+	 ('NVIDIA GeForce GTX 1660','2','3','1','4','1',NULL),
+	 ('AMD Radeon RX 6700 XT','5','5','3','1','3',NULL),
+	 ('AMD Radeon RX 6600 XT','4','4','3','3','3',NULL),
+	 ('AMD Radeon RX 6500 XT','4','3','3','4','3',NULL),
+	 ('AMD Radeon RX 5700 XT','3','1','3','3','2',NULL),
+	 ('AMD Radeon RX 5700','3','4','3','3','3',NULL);
+INSERT INTO uts_spk.bobot (nama,clock_speed,bandwidth,vram,harga,series,bobot) VALUES
+	 ('Bobot Kepentingan','4','4','3','5','3',NULL);
---
+++ UTS/daftar_gpu.sql
@@ -0,0 +1,11 @@
+INSERT INTO uts_spk.gpu (nama,clock_speed,bandwidth,vram,harga,series) VALUES
+	 ('NVIDIA GeForce RTX 3090','1700 MHz','936 GB/s','24 GB','25000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3080 Ti','1665 MHz','936 GB/s','12 GB','20000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3080','1750 MHz','448 GB/s','8 GB','15000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3070 Ti','1770 MHz','672 GB/s','8 GB','12000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3070','1500 MHz','460 GB/s','8 GB','10000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3060 Ti','1665 MHz','384 GB/s','8 GB','7000000','RTX 30'),
+	 ('NVIDIA GeForce RTX 3060','1400 MHz','336 GB/s','12 GB','6000000','RTX 30'),
+	 ('AMD Radeon RX 6900 XT','2300 MHz','512 GB/s','16 GB','20000000','RX 6000'),
+	 ('AMD Radeon RX 6800 XT','2035 MHz','448 GB/s','16 GB','15000000','RX 6000'),
+	 ('AMD Radeon RX 6700 XT','2425 MHz','384 GB/s','12 GB','10000000','RX 6000');
